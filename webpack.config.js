ce1ed107d371123836ed137d36b664ff31bdbc5a
Fabian Sanchez <faalsaru@gmail.com>
2021-06-08 13:02:56 -0500

    :art: creating routes and router

--- webpack.config.js
+++ webpack.config.js
@@ -26,12 +26,12 @@ module.exports = {
     },
 
     plugins:[
-        new HtmlWebpackPlugin([
+        new HtmlWebpackPlugin(
             {
                 inject: true,
                 template: './public/index.html',
                 filename: './index.html'
             }
-        ]),
+        ),
     ]
 }
